699bdbba69fb7d9c8307ce7a05bf072e43c202fd
xiaobog <garsonguo@163.com>
2018-09-14 15:17:47 +0800

    引入iview

--- README.md
+++ README.md
@@ -19,3 +19,15 @@ yarn run build
 ```
 yarn run lint
 ```
+### 问题记录与开发创建步骤
+1. 创建与初始化项目
+1.1. 全局安装脚手架npm install -g @vue/cli，vue-cli3
+1.2. git新建仓库，clone到本地，创建项目vue create x-auth，覆盖掉本地仓库；
+1.3. vue-cli3官网：https://cli.vuejs.org/zh/guide/
+1.4. vue ui 启动VUE项目管理页，可以在里面管理你的项目
+2. vue-cli3中的配置文件 vue.config.js
+2.1. 
+3. 引入iview https://www.iviewui.com/docs/guide/install
+3.1. 按需引入导致iview not defined https://www.jianshu.com/p/0520f4775456
+3.2. vscode中iview的标签报错 x-invalid-end-tag 关掉vetur.validation.template: https://blog.csdn.net/weixin_38278878/article/details/80447503
+:https://segmentfault.com/q/1010000013227727?utm_source=tag-newest
---
+++ src/views/layout/Layout.vue
@@ -0,0 +1,63 @@
+<template>
+    <div>
+        <Layout>
+            <Sider ref="side1" hide-trigger collapsible :collapsed-width="78" v-model="isCollapsed">
+                <Menu active-name="1-2" theme="dark" width="auto" :class="menuitemClasses">
+                    <MenuItem name="1-1">
+                        <Icon type="ios-navigate"></Icon>
+                        <span>Option 1</span>
+                    </MenuItem>
+                    <MenuItem name="1-2">
+                        <Icon type="ios-search"></Icon>
+                        <span>Option 2</span>
+                    </MenuItem>
+                    <MenuItem name="1-3">
+                        <Icon type="ios-settings"></Icon>
+                        <span>Option 3</span>
+                    </MenuItem>
+                </Menu>
+            </Sider>
+            <Layout>
+                <Header :style="{padding: 0}" class="layout-header-bar">
+                    <Icon @click.native="collapsedSider" :class="rotateIcon" :style="{margin: '0 20px'}" type="md-menu" size="24"></Icon>
+                </Header>
+                <Content :style="{margin: '20px', background: '#fff', minHeight: '260px'}">
+                    Content
+                </Content>
+            </Layout>
+        </Layout>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                isCollapsed: false
+            }
+        },
+        computed: {
+            rotateIcon () {
+                return [
+                    'menu-icon',
+                    this.isCollapsed ? 'rotate-icon' : ''
+                ];
+            },
+            menuitemClasses () {
+                return [
+                    'menu-item',
+                    this.isCollapsed ? 'collapsed-menu' : ''
+                ]
+            }
+        },
+        methods: {
+            collapsedSider () {
+                this.$refs.side1.toggleCollapse();
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
