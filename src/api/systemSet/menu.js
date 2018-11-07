01a563ca50bd14ee7879ce1146b49394756635b9
xiaobog <garsonguo@163.com>
2018-11-07 17:24:44 +0800

    fix:菜单管理增改查实现

---
+++ src/api/systemSet/menu.js
@@ -0,0 +1,73 @@
+import axios from 'axios'
+
+const baseUrl = 'http://127.0.0.1:3000'
+
+/**
+ * @param {*} 查询参数
+ * @description 调用查询服务端接口
+ */
+export const queryList = (params) => {
+    let url = `${baseUrl}/systemSet/menu/queryList`
+    return new Promise((resolve, reject) => {
+        axios.get(url, {
+            params
+        }).then(res => {
+            if (res.status === 200) {
+                resolve(res.data.result)
+            } else {
+                reject(res)
+            }
+        })
+    })
+}
+/**
+ * @param {*} model
+ * @description 调用新增服务端接口
+ */
+export const add = (modal) => {
+    let url = `${baseUrl}/systemSet/menu/add`
+    return new Promise((resolve, reject) => {
+        axios.post(url, modal).then(res => {
+            if (res.status === 200) {
+                resolve(res)
+            } else {
+                reject(res)
+            }
+        })
+    })
+}
+/**
+ * @description 调用登删除服务端接口
+ */
+export const deleteMenu = (id) => {
+    let url = `${baseUrl}/systemSet/menu/delete`
+    let idObj = {
+        "id": id
+    }
+    return new Promise((resolve, reject) => {
+        axios.post(url, idObj).then(res => {
+            if (res.status === 200) {
+                resolve(res)
+            } else {
+                reject(res)
+            }
+        })
+    })
+}
+
+/**
+ * @param {*} model
+ * @description 调用编辑服务端接口
+ */
+export const edit = (model) => {
+    let url = `${baseUrl}/systemSet/menu/edit`
+    return new Promise((resolve, reject) => {
+        axios.post(url, model).then(res => {
+            if (res.status === 200) {
+                resolve(res)
+            } else {
+                reject(res)
+            }
+        })
+    })
+}
--- src/views/systemSet/MenuManage.vue
+++ src/views/systemSet/MenuManage.vue
@@ -1,5 +1,5 @@
 <style scoped lang="less">
-@import './menuManage.less';
+@import "./menuManage.less";
 </style>
 
 <template>
@@ -68,7 +68,7 @@
                                             <Option v-for="item in iconList" :value="item.value" :key="item.value">{{ item.label }}</Option>
                                         </Select>
                                     </FormItem>
-                                    <Button type="primary" class="btn" @click="menuSubmit">提交</Button>
+                                    <Button type="primary" class="btn" @click="menuSubmit" :disabled="btnDisable">提交</Button>
                                 </Form>
                             </Col>
                         </Row>
@@ -80,123 +80,200 @@
 </template>
 
 <script>
-import { saveMenu } from '../../service/systemSet.js'
-    export default {
-        data() {
-            return {
-                subDis: false,
-                topOrSub: true,
-                menuTitle: '菜单',
-                menuTreeList:[{
-                    title: '系统',
-                    expand: true,
-                    children:[{
-                        id:'1',
-                        title:'系统设置',
-                        children: [{
-                            id:'1-1',
-                            title:'菜单管理'
-                        }]
-                    },{
-                        title:'权限管理',
-                        id:'2',
-                        children: [{
-                            id:'2-1',
-                            title:'功能管理'
-                        },{
-                            id:'2-2',
-                            title:'角色管理'
-                        },{
-                            id:'2-3',
-                            title:'角色权限管理'
-                        },{
-                            id:'2-4',
-                            title:'角色用户管理'
-                        },{
-                            id:'2-5',
-                            title:'用户角色管咯'
-                        }]
-                    },{
-                        title:'组织构架',
-                        id:'3',
-                        children: [{
-                            id:'3-1',
-                            title:'部门管理'
-                        },{
-                            id:'3-2',
-                            title:'职位管理'
-                        }]
-                    },{
-                        title:'用户管理',
-                        id:'4',
-                        children: [{
-                            id:'4-1',
-                            title:'用户管理'
-                        }]
-                    }]
-                }],
-                iconList: [{
-                    value: 'icon-fa',
-                    label: 'icon-fa'
-                }],
-                systemForm: {
-                    name: '',
-                    title: '',
-                    authCode: '',
-                    sort: 0,
-                    leftShow: false,
-                    lock: true,
-                    icon: ''
+import { add, edit, deleteMenu } from "../../api/systemSet/menu.js";
+export default {
+  data() {
+    return {
+      isTop: false, // 是否为添加顶级菜单 false->添加子菜单
+      subDis: false,
+      isEdit: false, // 是否为编辑
+      topOrSub: true,
+      btnDisable: true, // 初始化页面是，禁用提交按钮
+      menuTitle: "菜单",
+      currentId: "", // 当前选中菜单的ID
+      menuTreeList: [
+        {
+          title: "系统",
+          expand: true,
+          children: [
+            {
+              id: "1",
+              title: "系统设置",
+              children: [
+                {
+                  id: "1-1",
+                  title: "菜单管理"
+                }
+              ]
+            },
+            {
+              title: "权限管理",
+              id: "2",
+              children: [
+                {
+                  id: "2-1",
+                  title: "功能管理"
                 },
-                systemRules:{
-                    name:[{ required: true, message: '名称不能为空', trigger: 'blur' }],
-                    title:[{ required: true, message: '标题不能为空', trigger: 'blur' }]
+                {
+                  id: "2-2",
+                  title: "角色管理"
+                },
+                {
+                  id: "2-3",
+                  title: "角色权限管理"
+                },
+                {
+                  id: "2-4",
+                  title: "角色用户管理"
+                },
+                {
+                  id: "2-5",
+                  title: "用户角色管咯"
                 }
+              ]
+            },
+            {
+              title: "组织构架",
+              id: "3",
+              children: [
+                {
+                  id: "3-1",
+                  title: "部门管理"
+                },
+                {
+                  id: "3-2",
+                  title: "职位管理"
+                }
+              ]
+            },
+            {
+              title: "用户管理",
+              id: "4",
+              children: [
+                {
+                  id: "4-1",
+                  title: "用户管理"
+                }
+              ]
             }
-        },
-        methods: {
-            initData() {
-                return {
-                    name: '',
-                    title: '',
-                    authCode: '',
-                    sort: 0,
-                    leftShow: false,
-                    lock: true,
-                    icon: ''
-                }
-            },
-            handleTopMenu() {
-                this.subDis = !this.subDis
-                this.menuTitle = '正在添加顶级菜单'
-                this.systemForm = initData()
-            },
-            handleSubMenu() {
-                this.menuTitle = '正在添加子级菜单'
-                this.systemForm = initData()
-            },
-            handleDeleteMenu() {},
-            selectChange(params) {
-                let title = params[0].title
-                let id = params[0].id
-                this.menuTitle = `正在编辑【${title}】`
-                this.systemForm = {
-                    name: title,
-                    title: title,
-                    authCode: 1,
-                    sort: 0,
-                    leftShow: true,
-                    lock: true,
-                    icon: 'icon-fa'
-                }
-            },
-            menuSubmit() {
-                this.$refs.systemForm.validate((valid)=>{
-                    if(valid){
-                      saveMenu(this.systemForm)
-                    }
-                })
-            }
+          ]
         }
+      ],
+      iconList: [
+        {
+          value: "icon-fa",
+          label: "icon-fa"
+        }
+      ],
+      systemForm: {
+        parentId: 0,
+        name: "",
+        title: "",
+        authCode: "",
+        sort: 0,
+        leftShow: false,
+        lock: true,
+        icon: ""
+      },
+      systemRules: {
+        name: [{ required: true, message: "名称不能为空", trigger: "blur" }],
+        title: [{ required: true, message: "标题不能为空", trigger: "blur" }]
+      }
+    };
+  },
+  methods: {
+    initData() {
+      return {
+        name: "",
+        title: "",
+        authCode: "",
+        sort: 0,
+        leftShow: false,
+        lock: true,
+        icon: ""
+      };
+    },
+    handleTopMenu() {
+      this.isTop = true;
+      this.btnDisable = false;
+      this.subDis = !this.subDis;
+      this.isEdit = false;
+      this.menuTitle = "正在添加顶级菜单";
+      this.systemForm = this.initData();
+    },
+    handleSubMenu() {
+      this.isTop = false;
+      this.isEdit = false;
+      this.menuTitle = "正在添加子级菜单";
+      this.systemForm = this.initData();
+    },
+    handleDeleteMenu() {
+      deleteMenu(this.currentId).then(res => {
+        if (res.status === 200) {
+          //   this.menuTreeList.splice(params.index, 1);
+          this.$Message.success("删除成功!");
+        } else {
+          this.$Message.success("删除失败!");
+        }
+      });
+    },
+    selectChange(params) {
+      this.isEdit = true;
+      this.subDis = false;
+      this.btnDisable = false;
+      let title = params[0].title;
+      this.currentId = params[0].id;
+      this.menuTitle = `正在编辑【${title}】`;
+      this.systemForm = {
+        id: this.currentId,
+        name: title,
+        title: title,
+        authCode: 1,
+        sort: 0,
+        leftShow: true,
+        lock: true,
+        icon: "icon-fa"
+      };
+    },
+    menuSubmit() {
+      this.$refs.systemForm.validate(valid => {
+        if (valid) {
+          if (this.isTop && !this.isEdit) {
+            this.systemForm.parentId = 0;
+            add(this.systemForm).then(res => {
+              if (res.status === 200) {
+                this.modalShow = false;
+                this.menuTreeList.push(res.data.result);
+                this.$Message.success("提交成功!");
+              } else {
+                this.$Message.error("提交失败!");
+              }
+            });
+          } else if (!this.isTop && !this.isEdit) {
+            this.systemForm.parentId = this.currentId;
+            add(this.systemForm).then(res => {
+              if (res.status === 200) {
+                this.modalShow = false;
+                this.menuTreeList.push(res.data.result);
+                this.$Message.success("提交成功!");
+              } else {
+                this.$Message.error("提交失败!");
+              }
+            });
+          } else {
+            debugger;
+            edit(this.systemForm).then(res => {
+              if (res.status === 200) {
+                this.modalShow = false;
+                this.$Message.success("提交成功!");
+              } else {
+                this.$Message.error("提交失败!");
+              }
+            });
+          }
+        }
+      });
     }
+  }
+};
 </script>
