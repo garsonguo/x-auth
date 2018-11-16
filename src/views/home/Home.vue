f3ec6f26139639a9abf69e62f42f59e65c0ff9ab
xiaobog <garsonguo@163.com>
2018-11-16 17:09:16 +0800

    fix：添加首页简介

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,18 @@
 <template>
     <div>
-        woshihome
+        系统简介：
+        <p>系统前台：基于vue全家桶实现，github：
+            <a href="https://github.com/garsonguo/x-auth" target="_blank">https://github.com/garsonguo/x-auth</a>
+        </p>
+        <p>系统后台：基于koa2+lowdb实现，github：
+            <a href="https://github.com/garsonguo/x-node" target="_blank">>https://github.com/garsonguo/x-node</a>
+        </p>
     </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {};
 </script>
 
 <style scoped>
-
 </style>
