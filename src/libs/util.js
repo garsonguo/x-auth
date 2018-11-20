067a7ef45c02e0903fe48e717f5e65dae8751ae6
xiaobog <garsonguo@163.com>
2018-11-20 13:42:32 +0800

    bug修改

--- src/libs/util.js
+++ src/libs/util.js
@@ -12,7 +12,7 @@ export const getNewTagList = (currentList, newRoute) => {
     } = newRoute
     // 相当于复制了一个数组
     let newlist = [...currentList]
-    if(name==='Home'){
+    if (name === 'Home') {
         return newlist
     }
     if (newlist.findIndex(item => item.name === name) >= 0) {
@@ -30,7 +30,7 @@ export const getNewTagList = (currentList, newRoute) => {
  * @param {*} tagName 要关闭的标签名
  * @description {*} 过滤掉关闭的标签名
  */
-export const closeTags = (currentList,tagName) => {
+export const closeTags = (currentList, tagName) => {
     let newlist = [...currentList]
     newlist = newlist.filter((value) => {
         return value.name != tagName
@@ -42,7 +42,7 @@ export const closeTags = (currentList,tagName) => {
  * @param {*} tagName 要保留的标签名
  * @description {*} 关闭其他保留标签名
  */
-export const keepTags = (currentList,tagName) => {
+export const keepTags = (currentList, tagName) => {
     let newlist = [...currentList]
     newlist = newlist.filter((value) => {
         return value.name === tagName
@@ -54,17 +54,21 @@ export const keepTags = (currentList,tagName) => {
  * @param {*} tagList tags打开的标签列表
  * @description {*} 将taglist存储到cookie，实现刷新浏览器，tag打开的标签不变
  */
-export const setTagCookie = (tagList) =>{
+export const setTagCookie = (tagList) => {
     Cookies.remove('tagList')
-    Cookies.set('tagList',tagList)
+    Cookies.set('tagList', tagList)
 }
 
 /** 
  * @param {*} tagList tags打开的标签列表
  * @description {*} 获取cookie中的taglist
  */
-export const getTagCookie = () =>{
+export const getTagCookie = () => {
     let result = []
-    result = Cookies.get('tagList')
+    let tagList = Cookies.get('tagList')
+    if (!tagList) {
+        tagList = "[]"
+    }
+    result = tagList
     return JSON.parse(result)
 }
